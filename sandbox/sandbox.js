3ee9753e8dbab1f6deb40ecbed0f9e0d37d169b4
David Neilsen <d.neilsen@we-are-mea.com>
2016-10-27 07:55:43 +1300

    Test ContentTools in an iframe

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -142,7 +142,8 @@
     ContentTools.IMAGE_UPLOADER = ImageUploader.createImageUploader;
     ContentTools.StylePalette.add([new ContentTools.Style('By-line', 'article__by-line', ['p']), new ContentTools.Style('Caption', 'article__caption', ['p']), new ContentTools.Style('Example', 'example', ['pre']), new ContentTools.Style('Example + Good', 'example--good', ['pre']), new ContentTools.Style('Example + Bad', 'example--bad', ['pre'])]);
     editor = ContentTools.EditorApp.get();
-    editor.init('[data-editable], [data-fixture]', 'data-name');
+    var nodes = document.querySelector('iframe').contentDocument.querySelectorAll('[data-editable], [data-fixture]');
+    editor.init(nodes, 'data-name');
     editor.addEventListener('saved', function(ev) {
       var saved;
       console.log(ev.detail().regions);
